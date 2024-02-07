823457dc6afb0b09ec071e571ade7ece92fa011d
LeiderIP <leiderpaez13@gmail.com>
2024-02-07 11:52:53 -0500

    all three tasks updated

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,36 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+document.querySelector('.form').addEventListener('submit', event => {
+  event.preventDefault();
+
+  const delayInput = Number(event.currentTarget.delay.value);
+  const stepInput = Number(event.currentTarget.step.value);
+  const amountInput = Number(event.currentTarget.amount.value);
+
+  for (let i = 1; i <= amountInput; i++) {
+    createPromise(i, delayInput + stepInput * (i - 1))
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
   }
+});
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
